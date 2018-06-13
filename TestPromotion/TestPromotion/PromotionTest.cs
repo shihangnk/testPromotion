375bd9c2b99a60f209304b2c9179c723c76a4cbd
SeanShi <seans@iqmetrix.com>
2018-06-13 17:48:02 -0600

    init

---
+++ TestPromotion/TestPromotion/PromotionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestPromotion
+{
+    [TestClass]
+    public class PromotionTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var promotions = getPromotions();
+
+            Console.Out.WriteLine("......." + promotions.Result);
+
+            Assert.AreSame("aaaaa", promotions.Result);
+        }
+
+        async Task<string> getPromotions()
+        {
+            return await getPromotionsAsync();
+        }
+
+        async Task<string> getPromotionsAsync()
+        {
+            await Task.Delay(1000);
+            return "aaaaa";
+        }
+    }
+}
---
+++ TestPromotion/TestPromotion/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestPromotion")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPromotion")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5f63e996-6647-4121-85dc-8a8c9ecf8b40")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
