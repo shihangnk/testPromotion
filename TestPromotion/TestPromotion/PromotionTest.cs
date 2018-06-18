3ca0804b456d4d69251cf81e775030425eb0055b
SeanShi <seans@iqmetrix.com>
2018-06-18 16:26:23 -0600

    remove unnecessary debug.

--- TestPromotion/TestPromotion/PromotionTest.cs
+++ TestPromotion/TestPromotion/PromotionTest.cs
@@ -17,8 +17,10 @@ namespace TestPromotion
     public class PromotionTest
     {
         private static readonly ILog Logger = LogManager.GetLogger(typeof(PromotionTest));
-//      private const string url = "http://localhost:61284/v1/Companies(272002)/promotions";
-        private const string _url = "https://apiint.iqmetrix.net/pospromotions/v1/companies(272002)/promotions";
+        //      private const string url = "http://localhost:61284/v1/Companies(272002)/promotions";
+        private const string _urlBase = "https://apiint.iqmetrix.net/pospromotions/v1/companies(272002)";
+        private const string _url = _urlBase+"/promotions";
+        private const string _getActivePromotions = _urlBase + "/entities(337730)/ActivePromotionsForDays(2018-05-14, 30)";
 
         [TestMethod]
         public void TestMethod1()
@@ -49,7 +51,6 @@ namespace TestPromotion
         private void ClearPromotion()
         {
             List<Promotion> promotions = GetPromotions();
-            Assert.IsTrue(GetPromotions().Count>0, "Get all promotions");
             foreach (var promotion in promotions)
             {
                 ServiceCaller.Invoke("DELETE", $"{_url}({promotion.Id})", "");
@@ -60,8 +61,6 @@ namespace TestPromotion
         private List<Promotion> GetPromotions()
         {
             var str = ServiceCaller.Invoke("GET", _url, "");
-            Debug.WriteLine("......." + str);
-
             List<Promotion> promotions = JsonConvert.DeserializeObject<List<Promotion>>(str);
             return promotions;
         }
